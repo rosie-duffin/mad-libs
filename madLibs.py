fe7b09bce567e7a4c5b2441ce0dd752caeea50f7
Rosie Duffin <72879135+rosie-duffin@users.noreply.github.com>
2021-09-06 11:27:35 +0100

    Add files via upload

---
+++ madLibs.py
@@ -0,0 +1,25 @@
+loop=1
+
+while(loop<10):
+    place=input("Choose a place you would go on holiday: ")
+    adjective1=input("Choose an adjective: ")
+    verb1=input("Choose a verb: ")
+    noun1=input("Choose a noun: ")
+    familymember=input("Choose a family member: ")
+    noun2=input("Choose a noun: ")
+    adverb=input("Choose an adverb: ")
+    verb2=input("Choose a past-tense verb: ")
+    bodypart=input("Choose a body part: ")
+    emotion=input("Choose an emotion: ")
+    adjective2=input("Choose an adjective: ")
+    adjective3=input("Choose an adjective: ")
+    season=input("Choose a season: ")
+    
+    print("--------------------------")
+    print("The",place,"was",adjective1,"and we couldn't wait to",verb1,"down to the",noun1,".") 
+    print("My",familymember,"made me wait to put on plenty of",noun2,",")
+    print("whilst I",adverb,verb2,"from",bodypart,"to",bodypart,"with",emotion,".")
+    print("The",noun1,"was lovely and",adjective2,"- it was the",adjective3,season,"day.")
+    print("--------------------------")
+    
+    loop=loop+1
